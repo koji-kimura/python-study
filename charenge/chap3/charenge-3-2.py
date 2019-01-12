47b9628fe0e03e46ee7f5eb26213a90b8a77b44c
k-kimura <kimura_511@me.com>
2019-01-12 09:04:54 +0900

    1月12日勉強分

---
+++ charenge/chap3/charenge-3-2.py
@@ -0,0 +1,5 @@
+x = 9
+if x <10:
+  print("less 10")
+elif x > 10:
+  print("more 10")
---
+++ charenge/chap3/charenge-3-3.py
@@ -0,0 +1,7 @@
+x = 24
+if x <10:
+  print("less 10")
+elif (x > 10) & (x <= 25):
+  print("more 10 less 25")
+elif x > 25:
+  print("more 25")
