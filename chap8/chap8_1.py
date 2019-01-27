ce5402393a407fa52f654b3e9e9506873fd11b09
k-kimura <kimura_511@me.com>
2019-01-28 08:21:18 +0900

    本日勉強分

---
+++ chap8/chap8_1.py
@@ -0,0 +1,8 @@
+import math
+import random
+
+
+x = math.pow(2,3)
+y = random.randint(0,10)
+print(x)
+print(y)
---
+++ chap8/chap8_2.py
@@ -0,0 +1,11 @@
+import statistics
+import math
+nums = [1,5,33,12,46,33,2]
+
+x = statistics.mean(nums)
+y = statistics.median(nums)
+z = statistics.mode(nums)
+
+print(x)
+print(y)
+print(z)
