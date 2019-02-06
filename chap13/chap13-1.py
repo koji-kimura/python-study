43888548c475f8e922940c0eb57b687ed861e252
k-kimura <kimura_511@me.com>
2019-02-07 08:21:20 +0900

    本日作業分

---
+++ chap13/chap13-1.py
@@ -0,0 +1,14 @@
+class Reactagle:
+  def __init__(self,w,l):
+    self.width=w
+    self.len=1
+
+  def area(self):
+    return self.width * self*len
+
+class Data:
+  def __init__(self):
+    self.num = [1,2,3,4,5]
+
+  def change_data(self,index,n):
+    self.nums[index] = n
